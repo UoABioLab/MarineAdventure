c00ceea7a5f8e03f58be7e73a37d34d875677055
m183znuc <18318734461@163.com>
2024-11-20 12:14:15 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/GameIndex/',
+  plugins: [vue()],
+  server: {
+    port: 9205
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mediapipe-pose': ['@mediapipe/pose'],
+          'tensorflow': ['@tensorflow/tfjs-core', '@tensorflow/tfjs-backend-webgl']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['@mediapipe/pose', '@tensorflow/tfjs-core', '@tensorflow/tfjs-backend-webgl']
+  }
+})
